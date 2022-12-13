f2c2104869732981bc6dfa0e62e64da4380fc9e8
이소영 <th42500@naver.com>
2022-12-13 11:03:15 +0900

    [Feat] AccountContoller - 회원가입 메서드 생성 및 Controller 테스트 진행
    Spring Security가 현재 적용되고 있으므로 401 에러 발생

---
+++ src/main/java/com/spring/jpa/account/AccountController.java
@@ -0,0 +1,17 @@
+package com.spring.jpa.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        return "account/sign-up";
+    }
+}
