45a5d7deacb24836d4d42232da3dbbcb4f56596d
이소영 <th42500@naver.com>
2022-12-18 23:53:01 +0900

    [Feat] JSR 303 Annotation 검증 구현

--- src/main/java/com/spring/jpa/account/AccountController.java
+++ src/main/java/com/spring/jpa/account/AccountController.java
@@ -1,19 +1,36 @@
 package com.spring.jpa.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/account")
 public class AccountController {
 
-
+    private final SignUpFormValidator signUpFormValidator;
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
 
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors) {
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // Todo 회원 가입 처리
+        return "redirect:/";
     }
 }
